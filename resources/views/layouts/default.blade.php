853c826d4807af062237fd0078bf72b9e7b38efe
ing <inyoman_gurnitha@yahoo.com>
2020-03-05 10:41:56 +0700

    6. Added html tempate to home page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <title>Document</title>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   <title>E-SHOP HTML Template</title>
+   <!-- Google font -->
+   <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+   <!-- Bootstrap -->
+   <link type="text/css" rel="stylesheet" href="{{ asset('e-shop/css/bootstrap.min.css') }}" />
+   <!-- Slick -->
+   <link type="text/css" rel="stylesheet" href="{{ asset('e-shop/css/slick.css') }}" />
+   <link type="text/css" rel="stylesheet" href="{{ asset('e-shop/css/slick-theme.css') }}" />
+   <!-- nouislider -->
+   <link type="text/css" rel="stylesheet" href="{{ asset('e-shop/css/nouislider.min.css') }}" />
+   <!-- Font Awesome Icon -->
+   <link rel="stylesheet" href="{{ asset('e-shop/css/font-awesome.min.css') }}">
+   <!-- Custom stlylesheet -->
+   <link type="text/css" rel="stylesheet" href="{{ asset('e-shop/css/style.css') }}" />
+   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <![endif]-->
 </head>
 <body>
    @include('partials.header')
+   @include('partials.navigation')
    <div>
       @yield('main')
    </div>
    @include('partials.footer')
+   <!-- jQuery Plugins -->
+   <script src="{{ asset('e-shop/js/jquery.min.js') }}"></script>
+   <script src="{{ asset('e-shop/js/bootstrap.min.j') }}s"></script>
+   <script src="{{ asset('e-shop/js/slick.min.js') }}"></script>
+   <script src="{{ asset('e-shop/js/nouislider.min.js') }}"></script>
+   <script src="{{ asset('e-shop/js/jquery.zoom.min.js') }}"></script>
+   <script src="{{ asset('e-shop/js/main.js') }}"></script>
 </body>
 </html>
